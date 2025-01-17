d3c8d8265a6a6a4b48702ec9f98d42aee4e33391
behruz <bahtiyorov757@gmail.com>
2025-01-17 14:38:10 +0500

    three commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import Imgg from './components/Imgg'
 import './App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Imgg/>
     </>
   )
 }
---
+++ src/components/Imgg.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Imgg = () => {
+  return (
+    <div className='dd'>
+      
+    </div>
+  )
+}
+
+export default Imgg
