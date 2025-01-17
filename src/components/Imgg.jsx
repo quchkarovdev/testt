a5edd8e210f622e3729ed4787507285d766276ca
behruz <bahtiyorov757@gmail.com>
2025-01-17 15:04:52 +0500

    four commit

--- src/components/Imgg.jsx
+++ src/components/Imgg.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Imgg = () => {
   return (
-    <div className='dd'>
-      
+    <div className='bg-black'>
+      <div>
+        <p className='text-2xl text-gray-700 text-center'>Про компанию</p>
+        <h1 className='text-4xl font-manrope text-white text-center'>NOVO - торговая площадка коммерческого <br /> транспорта и тяжелой техники</h1>
+      </div>
+      <div className='justfiy-center'>
+        <div className='as'><h6>Коммерческий
+        транспорт</h6></div>
+        <div className='as'><h6>Сельхоз
+        техника</h6></div>
+        <div className='as'><h6>Строительная
+        техника</h6></div>
+        <div className='as'><h6>Складское
+        оборудование</h6></div>
+      </div>
     </div>
   )
 }
